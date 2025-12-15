149478d40d7a49687c02c9be7e83d66f5aba9a73
mitch <larosa.mhl@gmail.com>
2025-12-15 15:26:35 +0100

    Iniziando a generare gerarchia finale controller e testando @PreAuthorize

--- src/main/java/twentyfive/appcovo2/apiGateway/FeRequestController.java
+++ src/main/java/twentyfive/appcovo2/apiGateway/FeRequestController.java
@@ -9,7 +9,7 @@ import twentyfive.appcovo2.requests.LoginRequest;
 import twentyfive.appcovo2.requests.RegistrationRequest;
 
 @RestController
-@RequestMapping("/openapi")
+@RequestMapping("/api")
 public class FeRequestController {
 
     @Autowired
@@ -18,19 +18,26 @@ public class FeRequestController {
     @PostMapping("/register")
     public ResponseEntity<Void> registerPlayer(@RequestBody RegistrationRequest registrationRequest) {
         feRequestService.registerPlayer(registrationRequest);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.noContent().build();
     }
 
     @PostMapping("/token")
     public ResponseEntity<String> getToken(@RequestBody LoginRequest loginRequest) {
         String token = feRequestService.getTokenLogin(loginRequest);
-        return new ResponseEntity<String>(token, HttpStatus.FOUND);
+        return ResponseEntity.ok(token);
     }
 
     @GetMapping("/prova")
     @PreAuthorize("hasRole('PLAYER')")
     public ResponseEntity<String> getProva() {
         return new ResponseEntity<>("sciao belo", HttpStatus.FOUND);
+    }
+
+    @PostMapping("/saveShop")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Void> saveShop(@RequestBody RegistrationRequest registrationRequest) {
+        feRequestService.registerShop(registrationRequest);
+        return ResponseEntity.noContent().build();
     }
 
 }
